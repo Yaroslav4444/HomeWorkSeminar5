04219dcc60348e9780b81f60054df42432682594
Yaroslav <perec-polosatb1i@list.ru>
2022-10-09 20:00:51 +0400

    a task 34

--- task_34/Program.cs
+++ task_34/Program.cs
@@ -9,7 +9,7 @@ int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
 Console.WriteLine("Массив: ");
-PrintArray(numbers);
+printArray(numbers);
 int count = 0;
 
 for (int j = 0; j < numbers.Length; j++)
@@ -25,13 +25,16 @@ void FillArrayRandomNumbers(int[] numbers)
         numbers[i] = new Random().Next(100, 1000);
     }
 }
-void PrintArray(int[] numbers)
+void printArray(int[] incomingArray)
 {
-    Console.Write("[ ");
-    for (int i = 0; i < numbers.Length; i++)
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
     {
-        Console.Write(numbers[i] + " ");
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(",");
+        }
     }
-    Console.Write("]");
-    Console.WriteLine();
+    Console.WriteLine("]");
 }
